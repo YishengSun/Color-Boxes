6898bdb38b4c38a795926d8a7e7c4b16c2d8f8f8
YishengSun <yisheng4@illinois.edu>
2020-01-15 11:49:11 -0600

    Updates

---
+++ precache-manifest.bb9ad3e4fdbd20f43c5c01ee73a655ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "939cb2c17209e0abb6d5d91df272e60d",
+    "url": "/Color-Boxes/index.html"
+  },
+  {
+    "revision": "1d690ab4be1d6fbbb007",
+    "url": "/Color-Boxes/static/css/main.f73d4dac.chunk.css"
+  },
+  {
+    "revision": "c12da648722e46ab7bb3",
+    "url": "/Color-Boxes/static/js/2.c506d3cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Color-Boxes/static/js/2.c506d3cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d690ab4be1d6fbbb007",
+    "url": "/Color-Boxes/static/js/main.e128aea1.chunk.js"
+  },
+  {
+    "revision": "9ae4c92c165bea82650b",
+    "url": "/Color-Boxes/static/js/runtime-main.b1fe2722.js"
+  }
+]);
